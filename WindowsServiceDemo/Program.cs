198965aacc3e4f7228b9b62000e4dfcdfa4ec2ee
Jack <xszaboj@gmail.com>
2016-06-13 12:30:31 +0100

    demo

---
+++ WindowsServiceDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WindowsServiceDemo
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            var service = new Service1();
+#if DEBUG
+            service.DebugMode();
+            Thread.Sleep(Timeout.Infinite);
+#else
+           
+                log4net.Config.XmlConfigurator.Configure();
+                ServiceBase[] ServicesToRun;
+                ServicesToRun = new ServiceBase[] 
+            { 
+                service
+            };
+                ServiceBase.Run(ServicesToRun);
+            
+#endif
+        }
+    }
+}
---
+++ WindowsServiceDemo/Service1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsServiceDemo
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            //you logic will be here
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        internal void DebugMode()
+        {
+            //put your breakpoint here
+            this.OnStart(null);
+        }
+    }
+}
